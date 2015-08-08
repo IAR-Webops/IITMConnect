d658ec6e3d7014299432275350ac4a668c7a924e
Yash Murty <yashmurty@gmail.com>
2015-08-08 08:34:00 -0400

    Admin Edit Event - Places API

--- app/views/admin/eventmanagement_edit.blade.php
+++ app/views/admin/eventmanagement_edit.blade.php
@@ -85,9 +85,36 @@
 				            	<label class="text-right">Event Place :</label>
 				              </div>
 				              <div class="col-sm-12 col-md-8">
+				                <!--
 				                <input type="text" class="form-control" name="event_place" placeholder="Event Place" value="{{ $event->event_place }}">
+				              	-->
+		
+					            <!-- Field - Event Place Google Maps API -->
+				              	<input id="event_place" class="controls" type="text" placeholder="Enter a location" name="event_place"
+				              	value="{{ $event->event_place }}"
+				              	style="background-color: #fff; font-family: Roboto;
+								        font-size: 15px; font-weight: 300; margin-left: 12px;
+								        padding: 0 11px 0 13px; text-overflow: ellipsis; width: 350px;
+								        margin-top: 16px;">
+
+							    <div id="type-selector" class="controls" style="display:none">
+							      <input type="radio" name="type" id="changetype-all" checked="checked">
+							      <label for="changetype-all">All</label>
+
+							      <input type="radio" name="type" id="changetype-establishment">
+							      <label for="changetype-establishment">Establishments</label>
+
+							      <input type="radio" name="type" id="changetype-address">
+							      <label for="changetype-address">Addresses</label>
+
+							      <input type="radio" name="type" id="changetype-geocode">
+							      <label for="changetype-geocode">Geocodes</label>
+							    </div>
+							    <div id="map-canvas" style="height:500px; "></div>
+
 				              </div>				              
 				            </div>
+
 				            <!-- Field - Event Facebook Link -->
 				            <div class="form-group">
 				              <div class="col-sm-12 col-md-4 text-right">				            
@@ -194,4 +221,93 @@
 
 
 </script>
+@stop
+
+@section('jsmainbodycontent')
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
+
+<script>
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-33.8688, 151.2195),
+    zoom: 13
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('event_place'));
+
+  var types = document.getElementById('type-selector');
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', map);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map, marker);
+  });
+
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    google.maps.event.addDomListener(radioButton, 'click', function() {
+      autocomplete.setTypes(types);
+    });
+  }
+
+  setupClickListener('changetype-all', []);
+  setupClickListener('changetype-address', ['address']);
+  setupClickListener('changetype-establishment', ['establishment']);
+  setupClickListener('changetype-geocode', ['geocode']);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
+
+
 @stop
