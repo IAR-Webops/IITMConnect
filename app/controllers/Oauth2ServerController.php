d5c8d4e77af8d2f77072520fc8760924aeba8ce2
Yash Murty <yashmurty@gmail.com>
2016-02-08 14:46:33 +0530

    AccessToken response with BasicInfo

--- app/controllers/Oauth2ServerController.php
+++ app/controllers/Oauth2ServerController.php
@@ -73,17 +73,49 @@ class Oauth2ServerController extends BaseController {
 	    $accessTokenResponseString = json_encode($accessTokenResponse);
 		$accessTokenResponseJSON = json_decode($accessTokenResponseString);
 		// return gettype($accessTokenResponseJSON);
+		$access_token = $accessTokenResponseJSON->access_token;
 		// return $accessTokenResponseJSON->access_token;
 
-	    $oauth_access_token = DB::table('oauth_access_tokens')->where('id', $accessTokenResponseJSON->access_token)->first();
-	    if ($oauth_access_token->count()) {
-			
+	    $oauth_access_token = DB::table('oauth_access_tokens')->where('id', $access_token)->first();
+	    if (!empty($oauth_access_token)) {
+			$session_id = $oauth_access_token->session_id;	   
 
-			return $oauth_access_token;	   
+		    $oauth_session = DB::table('oauth_sessions')->where('id', $session_id)->first();
+		    if (!empty($oauth_session)) {
+		    	$owner_id = $oauth_session->owner_id;
+		    	// return $owner_id;
+
+		    	$user = User::where('id', '=', $owner_id)->first();
+		    	if (!empty($user)) {
+		    		// return var_dump($basic_info);
+		    		$accessTokenResponseJSON->rollno = $user->rollno;
+
+		    		$basic_info = BasicInfo::where('user_id', '=', $owner_id)->first();
+			    	if (!empty($basic_info)) {
+			    		// return var_dump($basic_info);
+			    		$accessTokenResponseJSON->firstname = $basic_info->firstname;
+			    		$accessTokenResponseJSON->lastname = $basic_info->lastname;
+			    		$accessTokenResponseJSON->department = $basic_info->department;
+			    		$accessTokenResponseJSON->email = $basic_info->email;
+			    		return json_encode($accessTokenResponseJSON);
+					} else {
+				    	return "Error : Oauth response failed at fetching name, email via BasicInfo";
+					}
+
+				} else {
+			    	return "Error : Oauth response failed at fetching rollno via User";
+				}
+
+				
+
+
+		    } else {
+		    	return "Error : Oauth response failed at fetching owner_id via oauth_session";
+		    }
 			
 
 	    } else {
-		    return $accessTokenResponse;
+		    return "Error : Oauth Response failed at fetching session_id via oauth_access_token";
 	    }
 
 	}
