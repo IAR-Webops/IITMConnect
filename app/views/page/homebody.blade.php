4961218ad6dd2281fc025ec448a736f8399825e4
Yash Murty <yashmurty@gmail.com>
2016-03-13 02:16:16 +0530

    Fix : Home Page typo

--- app/views/page/homebody.blade.php
+++ app/views/page/homebody.blade.php
@@ -6,7 +6,7 @@
 	<p>Welcome to <strong>#iitmconnect</strong></p>
 	<p>The purpose of this app is to help you to stay in touch with insti forever. <br>
 	Please fill in your details by choosing the forms from the menu on the left.</p> 
-	<p>We would really appriciate if you spread a word about this to your friends too.</p>
+	<p>We would really appreciate if you spread a word about this to your friends too.</p>
 	<p>With warm regards,<br>
 	I&AR Team</p>
 
