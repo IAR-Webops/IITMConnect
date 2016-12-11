cc62611388e3c6342ff03d1a9c1507a7854b7dcc
Yash Murty <yashmurty@gmail.com>
2016-12-12 00:53:16 +0530

    Login UI Revamp

--- app/views/account/signin.blade.php
+++ app/views/account/signin.blade.php
@@ -2,19 +2,24 @@
 
 @section('content')
 
-    @include('layout.login-top')    
-	
+    @include('layout.login-top')
+
+        
+        <div id="bg">
+			<img src="{{ URL::asset('img/iitmconnect_bg.jpg') }}" alt="">
+		</div>
+
     	<div class="container-fluid" id="loginpage">
 	    	<div class="row">
 	    		<div class="login col-md-12">
-			        <div class="login-screen ">				    			
+			        <div class="login-screen ">
 		    			<div class="col-sm-12 col-md-4 col-md-offset-4">
-			    			<div class="login-form">	
-				    			
+			    			<div class="login-form">
+
 					            <div class="text-center">
 									<img height="150" src="{{ URL::asset('img/IIT_Madras_Logo_300.png') }}">
 								</div>
-								<h4 class="bg-color-text text-center">
+								<h4 class="text-center">
 									Welcome to #iitmconnect
 								</h4>
 								<hr style="color:black; border-style:inset;">
@@ -33,42 +38,42 @@
 										<i class="fa fa-facebook"></i> Sign in using Facebook
 									</a>
 								</div>
-								<hr style="color:black; border-style:inset;">							
+								<hr style="color:black; border-style:inset;">
 
-								<div class="row bg-color-text">									
+								<div class="row">
 									<div class="col-sm-12 text-center">
 									<h4>
-										<a href="{{ URL::route('about-us') }}" target="_alt">
+										<a href="{{ URL::route('about-us') }}" target="_alt" style="color: #ECF0F1">
 											<span class="glyphicon glyphicon-info-sign" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="About Us"></span>
 										</a>
 										|
-										<a href="{{ URL::route('privacy-policy') }}" target="_alt">
+										<a href="{{ URL::route('privacy-policy') }}" target="_alt" style="color: #ECF0F1">
 											<span class="glyphicon glyphicon-briefcase" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Privacy Policy"></span>
 										</a>
 										|
-										<a href="{{ URL::route('report-issues') }}" target="_alt">
+										<a href="{{ URL::route('report-issues') }}" target="_alt" style="color: #ECF0F1">
 											<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true" data-toggle="tooltip" data-placement="bottom" title="Report Issue"></span>
 										</a>
 									</h4>
 
 									</div>
 								</div>
-								<p class="bg-color-text text-center">Kindly disable Ad Blockers if you face Sign In Issues</p>
+								<p class="text-center">Kindly disable Ad Blockers if you face Sign In Issues</p>
 
-					        </div>        	
+					        </div>
 				        </div>
 				    </div>
 	    		</div>
 	       	</div>
         </div>
-        <!-- /.container -->      
+        <!-- /.container -->
 
-   
+
 
     @include('layout.login-bottom')
 
 @stop
 
 @section('jscontent')
-	
+
 @stop
