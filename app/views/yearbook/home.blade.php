6ab50a81b7e60f82fe8a44d9c8e8192aa48e7669
Yash Murty <yashmurty@gmail.com>
2017-01-13 02:10:56 +0530

    Yearbook - Batch project question

--- app/views/yearbook/home.blade.php
+++ app/views/yearbook/home.blade.php
@@ -1,4 +1,4 @@
-@extends('page.home-without-leftnav')
+	@extends('page.home-without-leftnav')
 
 @section('mainbodycontent')
 
@@ -10,19 +10,7 @@
 			<p class="text-center">
 			  Graduating Year : <strong>{{ $basic_info->graduatingyear }}</strong> <a href="{{ URL::route('basic-info') }}" class="btn btn-sm btn-warning">Edit</a>
 			</p>
-			<p>
-				Would you like to order the Yearbook?<br>
-				@if(is_null($user_yearbook))
-					<span style="font-size:22px; font-weight:900;">Yes</span><br>
-					To change your answer, kindly fill your Yearbook entry first.
-				@else
-					<!-- Button trigger modal -->
-					<span style="font-size:22px; font-weight:900;">Yes</span>
-					<a class="" data-toggle="modal" data-target="#myModal">
-					(Click here to change response)
-					</a>
-				@endif
-			</p>
+
 			<hr>
 			<h6 class="text-center">Your Yearbook entry</h6>
 			<!-- Profile Photo  -->
@@ -46,6 +34,20 @@
 				</p>
 			@else
 			<form class="form-horizontal">
+				<div class="form-group" style="margin-bottom:15px;">
+					<strong for="profile_photo" class="col-sm-12 col-md-4 control-label">Yearbook Icons: </strong>
+					<div class="col-sm-12 col-md-8">
+						@if(empty($user_yearbook->insti_life_icons))
+						<img style="max-height:50px; max-width:50px; margin-right:10px;" src="http://1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png">
+						<img style="max-height:50px; max-width:50px; margin-right:10px;" src="http://1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png">
+						<img style="max-height:50px; max-width:50px; margin-right:10px;" src="http://1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png">
+						@else
+						<img style="max-height:50px; max-width:50px" src="{{ $basic_info->profile_image }}">
+						@endif
+						<br>
+						<a style="width:178px; margin-top:10px;" data-toggle="modal" data-target="#yearbookIconsModal" class="btn btn-inverse">Edit Yearbook Icons</a>
+					</div>
+				</div>
 				<div class="form-group">
 					<strong for="insti_remember_for" class="col-sm-12 col-md-4 control-label">I'll always remember Insti for: </strong>
 					<div class="col-sm-12 col-md-8">
@@ -65,11 +67,26 @@
 					</div>
 				</div>
 			</form>
-			<hr>
 			<div class="col-sm-12 text-center">
 				<a href="{{ url('/yearbook') }}/{{ Auth::user()->rollno }}/edit" class="btn btn-primary">Make changes to your Yearbook entry</a>
 			</div>
 			@endif
+			<br>
+			<hr>
+			<p>
+				Would you like to contribute to the Batch Project?
+				<br>
+				@if(is_null($user_yearbook))
+					<span style="font-size:22px; font-weight:900;">Yes</span><br>
+					To change your answer, kindly fill your Yearbook entry first.
+				@else
+					<!-- Button trigger modal -->
+					<span style="font-size:22px; font-weight:900;">Yes</span>
+					<a class="" data-toggle="modal" data-target="#myModal">
+					(Click here to change response)
+					</a>
+				@endif
+			</p>
 
         </div>
 
@@ -82,19 +99,40 @@
 		        <h4 class="modal-title" id="myModalLabel">Order Yearbook?</h4>
 		      </div>
 		      <div class="modal-body">
-		        Would you like to order the Yearbook?
+		        Would you like to contribute to the Batch Project?
 				<div class="row">
 					<div class="col-sm-12 col-md-6 col-md-offset-3" style="margin-top:20px;">
-						<a class="btn btn-block btn-success btn-lg">Yes, I would like to Order</a>
+						<a class="btn btn-block btn-success btn-lg">Yes, I would.</a>
 					</div>
 					<div class="col-sm-12 text-center" style="margin-top:20px;">
-						--- OR ---
+						OR
 					</div>
 					<div class="col-sm-12 col-md-6 col-md-offset-3" style="margin-top:20px;">
 						<a class="btn btn-block btn-danger btn-lg">No. I don't</a>
 					</div>
 				</div>
+				<p>
+					<br>
+					If you choose NO, you will not receive the Yearbook.
+				</p>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 
+		<!-- Modal -->
+		<div class="modal fade" id="yearbookIconsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Choose Yearbook Icons</h4>
+		      </div>
+		      <div class="modal-body">
+		        Choose three icons that best describe you.
 		      </div>
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
